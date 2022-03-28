dfde503d888ad621fa02c9c74c53e0fcf12c0aeb
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-03-28 09:18:35 +0100

    creation controller

---
+++ src/Controller/StageAlterController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StageAlterController extends AbstractController
+{
+    /**
+     * @Route("/", name="Home")
+     */
+    public function index(): Response
+    {
+        return $this->render('stage_alter/index.html.twig', [
+            'controller_name' => 'StageAlterController',
+        ]);
+    }
+}

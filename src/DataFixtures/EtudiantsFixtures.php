51f8a52f0ab3b964e92c0b2bfff36d2277205235
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-03-29 17:12:10 +0200

    essai d'ajout du jeu d'essai

--- src/DataFixtures/EtudiantsFixtures.php
+++ src/DataFixtures/EtudiantsFixtures.php
@@ -4,13 +4,25 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Etudiants;
 
 class EtudiantsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $data = new Etudiants();
+
+        $data->setNumeroEtudiant(123456);
+
+        $data->setNom("buil");
+
+        $data->setPrenom("Tiago");
+
+        $data->setDateNaiss("30/07/2002");
+
+        $data->setRegimeInscription("initiale");
+        
+        $manager->persist($data);
 
         $manager->flush();
     }

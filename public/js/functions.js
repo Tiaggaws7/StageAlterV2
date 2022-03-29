885355e1e05381498435140f64f7cbea3c63a998
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-03-29 20:48:18 +0200

    ajout de la selection dans la liste des étudiants

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,6 +2,11 @@ function switch_page(nom_page){
     document.location.href = nom_page;
 }
 
-function test(oui){
-    console.log(oui);
+function choisirEtudiant(numero){
+    etudiant = document.getElementById(numero);
+    parent = etudiant.parentNode;
+    for ( i = 0; i < parent.children.length; i++){
+        parent.children[i].style.background = 'rgb(255, 230, 200)'
+    }
+    etudiant.style.background = 'lightgrey';
 }

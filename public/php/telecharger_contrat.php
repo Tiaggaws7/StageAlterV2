e8850ab0cf2a00e52a7539231fb2e1821eb1c4fd
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-03-30 17:20:08 +0200

    ajout de telechargement des fichiers

--- public/php/telecharger_contrat.php
+++ public/php/telecharger_contrat.php
@@ -6,8 +6,10 @@
     if (file_exists($file_path)) {
         header('Content-Type: application/octet-stream');
         header("Content-Transfer-Encoding: Binary"); 
-        header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-        readfile($file_url); 
+        header("Content-disposition: attachment; filename=\"" . basename($file_path) . "\""); 
+        readfile($file_path); 
+    } else {
+        header('Location: https://127.0.0.1:8000/etablir_contrats');
     }
 
 ?>

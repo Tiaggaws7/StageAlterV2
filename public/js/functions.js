c0faa38162b866ee09cfc1e5ae004ea0e420be85
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-04-05 15:26:55 +0200

    formulaire cree

--- public/js/functions.js
+++ public/js/functions.js
@@ -7,6 +7,8 @@ function switch_page(nom_page){
 // Permet de savoir quel étudiant est selectionné
 let etudiantChoisit = "";
 
+let etudiantSansEspace = "";
+
 // Permet de sélectionner un étudiant
 function choisirEtudiant(numero){
     etudiant = document.getElementById(numero);
@@ -17,6 +19,8 @@ function choisirEtudiant(numero){
     etudiant.style.background = 'lightgrey';
     etudiantChoisit = etudiant.textContent;
     document.getElementById("etablir_contrats").innerHTML = "Contrat de " + etudiantChoisit;
+    sansEspace = etudiantChoisit.replace(/ /g, "");
+    etudiantSansEspace = sansEspace;
 }
 
 // Fonction utilisé afin d'envoyer le nom de l'étudiant selectionné en paramètre à une fonction php 
@@ -39,9 +43,13 @@ function telechargerContrat() {
 }
 
 // Permet de créer la fiche d'information 
-function creerContrat() {
-    //renvoyerVersPhp("https://127.0.0.1:8000/php/creer_contrat.php?nomPrenom=");
+function formulaireContrat() {
     document.location.href = "https://127.0.0.1:8000/creer_contrat";
+}
+
+function creerContrat(){
+    console.log( etudiantSansEspace );
+    document.location.href = "https://127.0.0.1:8000/php/creer_contrat.php?nomPrenom=" + etudiantSansEspace;
 }
 
 // Permet de modifier la fiche d'information 

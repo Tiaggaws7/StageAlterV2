97e9eeecd02f5a31b6ef411aeb7ebcb3935cd019
Tiago Buil <tbuil@iutbayonne.univ-pau.fr>
2022-04-06 09:31:45 +0200

    ajout de pop up en cours

--- public/php/supprimer_contrat.php
+++ public/php/supprimer_contrat.php
@@ -3,6 +3,20 @@
     // Récupération des données contenue dans l'url
     $nomPrenom = $_GET['nomPrenom'];
 
+    $file_path = "../contrats/" . $nomPrenom . "_contrat.pdf";
+
+    // if (file_exists($file_path)) {
+    //     // Si la fiche d'information de l'étudiant existe, le télécharge
+    //     header('Content-Type: application/octet-stream');
+    //     header("Content-Transfer-Encoding: Binary"); 
+    //     header("Content-disposition: attachment; filename=\"" . basename($file_path) . "\""); 
+    //     readfile($file_path); 
+
+    // } else {
+    //     // Si le fichier n'existe pas, on retourne à la page etablir_contrat
+    //     header('Location: https://127.0.0.1:8000/etablir_contrats');
+    // }
+    
     // Suppprime le fichier  
     unlink("../contrats/" . $nomPrenom . "_contrat.pdf");
 
